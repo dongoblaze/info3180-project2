cbba5c8cbea7f1d5c97cb22aa5efd1f0d2d83613
tam876 <tammylee816@yahoo.com>
2020-04-25 18:06:29 -0500

    add temp

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,20 +1,33 @@
 /* Add your Application JavaScript */
 Vue.component('app-header', {
-    template: `
+    template:`
+    <div id="nav">
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
-      <a class="navbar-brand" href="#">Lab 7</a>
+      <a class="navbar-brand" href="/"><i class="fa fa-instagram" >Photogram</i></a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
     
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
-          <li class="nav-item active">
-            <router-link class="nav-link" to="/">Home <span class="sr-only">(current)</span></router-link>
-          </li>
+        </ul>
+        <ul class="navbar-nav">
+            <li class="nav-item active">
+                <router-link class="nav-link" to="/">Home <span class="sr-only">(current)</span></router-link>
+            </li> 
+            <li class="nav-item active">
+                <router-link class="nav-link" to="/explore">Explore </router-link>
+            </li> 
+            <li class="nav-item active">
+                <router-link class="nav-link" to="">My Profile</router-link>
+            </li> 
+            <li class="nav-item active">
+                <router-link class="nav-link" to="/logout">Logout </router-link>
+            </li>  
         </ul>
       </div>
     </nav>
+    </div>
     `
 });
 
@@ -72,8 +85,28 @@ const NotFound = Vue.component('not-found', {
     }
 })
 
+const Login = Vue.component('login',{
+    template:`
+    <div class="d-flex justify-content-center">
+    <h2>Login </h2>
+    <form action="/api/auth/login" method= "POST" enctype="multipart/form-data" @submit.prevent="LoginForm">
+    <div class="">
+    <label>Username</label>
+    <input type="text"/>
+    <label>Password</label>
+    <input type="password"/>
+    <button type="submit" class="btn btn-success">Login</button>
+    </div>
+    </form>
+    </div>
+     `
+})
+
 const Logout= Vue.component('logout-form',{
-    template:`<div> </div>
+    template:`
+    <div class="d-flex justify-content-center"> 
+    <h2>Logout</h2>
+    </div>
     `,
     created: function() {
         let self = this;
@@ -108,6 +141,67 @@ const Logout= Vue.component('logout-form',{
     }
 });
 
+const Register = Vue.component('register',{
+    template:`
+    <div class="d-flex justify-content-center"> 
+    <h2>Register</h2>
+    <form action="/api/users/register" method="POST" enctype="multipart/form-data" @submit.prevent="UserRegistration">
+    <label>Username</label>
+    <input type="text"/>
+    <label>Password</label>
+    <input type="password"/>
+    <label>Firstname</label>
+    <input type= "text"/>
+    <label>Lastname</label>
+    <input type="text"/>
+    <label>Email</label>
+    <input type="email"/>
+    <label>Location</label>
+    <input type="text"/>
+    <label>Biography</label>
+    <textarea></textarea>
+    <label>Photo</label>
+    <input type="file" accept="image/*"/>
+    <br>
+    <button type="submit" class="btn btn-success">
+    </form>
+    </div>
+    `
+})
+
+const Post= Vue.component('post',{
+    template:`
+    <div class="d-flex justify-content-center"> 
+    <div> 
+    <h2>New Post</h2> 
+    <form action="/api/users/user_id/posts" method="POST" enctype = "multipart/form-data" @submit.prevent="PostForm"> 
+    <label>Photo</label>
+    <input type="file" accept="image/*">
+    <div>
+    <label>Caption</label>
+    <textarea placeholder="Write a Caption ..."></textarea>
+    <br>
+    <router-link to="/explore"><button type="submit" class="btn btn-success">Submit</button></router-link>
+    </div>
+    <div>
+    </form>
+    `
+})
+
+const Users =Vue.component('user_profile',{
+    template:`
+    <div class="d-flex justify-content-center">
+    <img class: "uphoto" v-bind:src=" "/>
+    <div class="info">
+       <h5>{{user.firstname}} {{user.lastname}}</h5>
+       {{user.location}}
+       <p>Member since: {{user.date}}</p>
+       {{user.bio}}
+       <button class="btn btn-success" v-on:click="follow" >Follow</button>
+    </div>
+    </div>
+    `,
+})
 // Define Routes
 const router = new VueRouter({
     mode: 'history',
